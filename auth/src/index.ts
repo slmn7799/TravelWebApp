d6255d86bc6f151e180c7acfaf136ea49f213581
salman <salmann9977@gmail.com>
2023-03-12 18:59:06 +0530

    adding ingress-nginx controller

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,8 +3,13 @@ import { json } from 'body-parser';
 
 const app = express();
 
-app.use( json );
+app.use( json() );
+
+app.get( '/api/users/cc', (req, res)=>{
+    // res.send('hi there');
+    res.send("hi")
+})
 
 app.listen( 3000, ()=>{
-    console.log("Auth service listening on port 3000");
+    console.log("Auth service listening on port 3000@@@@");
 })
