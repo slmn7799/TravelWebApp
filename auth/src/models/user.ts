e43331ffac724bc9e59fdb7a948c6f9da083531c
salman <salmann9977@gmail.com>
2023-03-14 22:55:57 +0530

    creating User model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+interface UserAttrs{
+    email: string,
+    password: string
+}
+
+interface UserModel extends mongoose.Model<UserDoc>{
+    build( attrs: UserAttrs) : UserDoc;
+}
+
+interface UserDoc extends mongoose.Document{
+    email: string,
+    password: string
+}
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        require: true
+    },
+    password:{
+        type: String,
+        require: true
+    }
+});
+
+userSchema.statics.build = ( attrs: UserAttrs) => {
+    return new User(attrs);
+}
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
