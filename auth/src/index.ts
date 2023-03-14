a4b2ab99c2279dd1124693080b2e685c86b8f0b2
salman <salmann9977@gmail.com>
2023-03-14 17:58:30 +0530

    small fix for async

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { json } from 'body-parser';
+import "express-async-errors";
 
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
