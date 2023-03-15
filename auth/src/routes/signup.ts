1a1a2758f228e449abdec02ed9d00d450dcdb6f5
salman <salmann9977@gmail.com>
2023-03-15 23:46:44 +0530

    common response JSON object for user

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -37,7 +37,8 @@ async (req: Request, res: Response)=>{
     const userJwt = jwt.sign({
       id: user.id,
       email: user.email
-    },'asdf');
+    },
+    process.env.JWT_KEY!);
 
     //Stored jwt in session
     req.session = {
